3a0d45135cf4d8826edfaffacfdb88a6946c32b8
Tommie55555 <36003450+Tommie55555@users.noreply.github.com>
2024-02-15 13:55:59 +0100

    DataHandler getFocus and getCalm work

--- DataHandler.js
+++ DataHandler.js
@@ -1,67 +1,71 @@
 const { Neurosity } = require("@neurosity/sdk");
 require("dotenv").config();
 
-var do_exit = false;
+// var do_exit = false;
 
-var start_time = (new Date()).getTime()
+// var start_time = (new Date()).getTime()
 
-var session_data = [[], [], [], [], [], [], [], []]
+// var session_data = [[], [], [], [], [], [], [], []]
 
 
-//TODO: remove check_for_exit and testfuncignoredis, and also do_exit, start_time, session_data
-const check_for_exit = async () => {
-    if ((new Date()).getTime() > start_time + 10000) { // .getTime() returns milliseconds
-        console.log("Exiting!");
-        exit(0) // Cannot find name exit, but that's okay,  because it works anyway lol (albeit with the wrong exit code)
-    }
-};
+// //TODO: remove check_for_exit and testfuncignoredis, and also do_exit, start_time, session_data
+// const check_for_exit = async () => {
+//     if ((new Date()).getTime() > start_time + 10000) { // .getTime() returns milliseconds
+//         console.log("Exiting!");
+//         exit(0) // Cannot find name exit, but that's okay,  because it works anyway lol (albeit with the wrong exit code)
+//     }
+// };
 
-const testfuncignoredis = async () => {
-    return;
-    await neurosity
-        .login({
-        email,
-        password
-        })
-        .catch((error) => {
-        console.log(error);
-        throw new Error(error);
-        });
-    console.log("Logged in");
+// const testfuncignoredis = async () => {
+//     return;
+//     await neurosity
+//         .login({
+//         email,
+//         password
+//         })
+//         .catch((error) => {
+//         console.log(error);
+//         throw new Error(error);
+//         });
+//     console.log("Logged in");
     
-    return;
-    console.log("running brainwaves test stuff, this function should probably return before this");
-        // raw, rawUnfiltered and several other options exist
-    neurosity.brainwaves("raw").subscribe((data) => {
+//     return;
+//     console.log("running brainwaves test stuff, this function should probably return before this");
+//         // raw, rawUnfiltered and several other options exist
+//     neurosity.brainwaves("raw").subscribe((data) => {
         
-        for (var i = 0; i < data.data.length; i++) {
-            for (var j = 0; j < data.data[i].length; j++) {
-                session_data[i].push(data.data[i][j])
-            }
-        }
+//         for (var i = 0; i < data.data.length; i++) {
+//             for (var j = 0; j < data.data[i].length; j++) {
+//                 session_data[i].push(data.data[i][j])
+//             }
+//         }
 
-        console.log(session_data);
+//         console.log(session_data);
         
-    });
+//     });
 
-    var exit_checker = setInterval(check_for_exit, 1000);
+//     var exit_checker = setInterval(check_for_exit, 1000);
 
-    console.log("exiting??")
-};
-
-
+//     console.log("exiting??")
+// };
 
 
-class DataHandler {
+
+/**
+ * DataHandler object. Needs "await dataHandler.init();" before use!
+ * @class
+ */
+export class DataHandler {
+    #password = ""; // private property
+
     constructor() {
+        this.deviceId = process.env.DEVICE_ID || "";
+        this.email = process.env.EMAIL || "";
+        this.#password = process.env.PASSWORD || "";
         
-        const this.deviceId = process.env.DEVICE_ID || "";
-        const this.email = process.env.EMAIL || "";
-        const this.password = process.env.PASSWORD || "";
         
-        
-        var this.currentFocus = 0;
-        var this.currentCalm = 0;
+        this.currentFocus = NaN;
+        this.currentCalm = NaN;
         
         const verifyEnvs = (email, password, deviceId) => {
             const invalidEnv = (env) => {
@@ -73,49 +77,59 @@ class DataHandler {
             }
         };
         
-        verifyEnvs(this.email, this.password, this.deviceId);
+        verifyEnvs(this.email, this.#password, this.deviceId);
         console.log(`${this.email} attempting to authenticate to ${this.deviceId}`);
         
-        const this.neurosity = new Neurosity({
-            this.deviceId
-        });
+        this.neurosity = new Neurosity({deviceId: this.deviceId});
 
         
 
+    } // end of constructor
+
+    async init() {
         await this.neurosity
             .login({
-                this.email,
-                this.password
+                email: this.email,
+                password: this.#password
             })
             .catch((error) => {
-            console.log(error);
-            throw new Error(error);
+                console.log(error);
+                throw new Error(error);
             });
         console.log("Logged in to Neurosity");
         
         this.neurosity.focus().subscribe((focus) => {
-            this.currentFocus = focus;
+            this.currentFocus = focus.probability;
         });
 
-        this.neurosity.calm().subscribe(({ calm }) => {
-            this.currentCalm = calm;
+        this.neurosity.calm().subscribe((calm) => {
+            this.currentCalm = calm.probability;
         });
 
-    } // end of constructor
-
-    async getFocus {
-        return currentFocus;
     }
 
-    async getCalm {
-        return currentCalm;
+    async getFocus() { // should probably use an average over a few seconds or smth
+        return this.currentFocus;
+    }
+
+    async getCalm() {
+        return this.currentCalm;
     }
 
 
 }
-  
-  // Usage:
-const dataHandlerTest = new DataHandler();
-  
 
-  
+async function testDataHandler() {
+    // Usage:
+    const dHandler = new DataHandler();
+    await dHandler.init();
+
+    for (let i = 0; i < 300; i++){
+        await new Promise(r => setTimeout(r, 1000));
+
+        console.log("calm: ", await dHandler.getCalm());
+        console.log("focus: ", await dHandler.getFocus());
+    }
+}
+
+testDataHandler();
