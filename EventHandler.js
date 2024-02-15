92efb0386a35b9499502d0f7218b06e79bb6e49d
Litebertia <o.duphorn@hotmail.com>
2024-02-15 11:10:40 +0100

    firstEventHandler
    Start of EventhHanlder and test in extension.js file

---
+++ EventHandler.js
@@ -0,0 +1,25 @@
+// import tommyskod
+
+export class EventHandler {
+    // Initilize variables
+    allowNotificationFocus = true 
+
+
+    checkFocus() {
+        
+    }
+    checkCalm() {
+
+    }
+
+
+}
+
+// While program is running and can send notification (just once)
+// If (Focus < 30)
+// Notify
+// Set allowNotificationFocus = 0
+// If (Calm < 30)
+// Notify
+// Set allowNotificationCalm = 0
+// A la mejor give generic example here
