864b6c57ba3bbe90fa5dfaf255e4ab016def9c27
Litebertia <o.duphorn@hotmail.com>
2024-02-15 12:20:32 +0100

    US2
    US2 almost done, needs implementation of datahandler

--- EventHandler.js
+++ EventHandler.js
@@ -1,19 +1,46 @@
 // import tommyskod
+const vscode = require('vscode')
 
-export class EventHandler {
+class EventHandler {
     // Initilize variables
     allowNotificationFocus = true 
-
+    allowNotificationCalm = true
 
     checkFocus() {
-        
+        var focus = 50 // Ska vara focus = DataHandler.getFocus
+        if (focus < 30 && this.allowNotificationFocus == true) {
+            const text = 'This program is using the neurosity crown to measure '+
+            'your live focus level. Your level recently dropped below 30% which might mean you are too '+
+            'unfucosed to be productive in your development. Please check the chat for advice on how to '+
+            'regain your focus.'
+            vscode.window.showInformationMessage('You seem to be unfucosed.', 'Show more').then(_=>{
+                vscode.window.showInformationMessage('Focus', {modal:true, detail:text})})
+            this.allowNotificationFocus = false
+        }
+        if (this.allowNotificationFocus == false && focus > 40) {
+            this.allowNotificationFocus = true
+        }
     }
     checkCalm() {
-
+        var calm = 50 // Ska vara calm = DataHandler.getCalm
+        if (calm < 60 && this.allowNotificationCalm == true) {
+            const text = 'This program is using the neurosity crown to measure '+
+            'your live calmness level. Your level recently dropped below 30% which might mean you are too '+
+            'agitated to be productive in your development. Please check the chat for advice on how to '+
+            'regain your calmness.'
+            vscode.window.showInformationMessage('You seem to be agitated.', 'Show more').then(_=>{
+                vscode.window.showInformationMessage('Calmness', {modal:true, detail:text})})
+            this.allowNotificationCalm = false
+        }
+        if (this.allowNotificationCalm == false && calm > 40) {
+            this.allowNotificationCalm = true
+        }
     }
 
 
 }
+
+module.exports = EventHandler
 
 // While program is running and can send notification (just once)
 // If (Focus < 30)
--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-import {EventHandler} from './EventHandler.js'
+const EventHandler = require('./EventHandler.js')
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
@@ -13,9 +13,7 @@ function activate(context) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "emoide" is now active!');
-	var EV = new EventHandler()
-	EV.allowNotificationFocus = false
-	console.log(EV.allowNotificationFocus)
+	
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
