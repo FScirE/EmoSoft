935eb9a0c98037ae0847d805abf2e0247c6f96fe
Tommie55555 <36003450+Tommie55555@users.noreply.github.com>
2024-02-16 12:45:37 +0100

    use CommonJS, make extention.js activate async

--- extension.js
+++ extension.js
@@ -1,6 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
+
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+
+const { DataHandler } = require('./DataHandler.js');
+
+
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,7 +14,7 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -23,6 +29,7 @@ function activate(context) {
 	//context.subscriptions.push(disposable);
 
 	vscode.window.showInformationMessage('Hello World from emoide!');
+
 }
 
 // This method is called when your extension is deactivated
