29f691c2acc0ba6d92ecbfbcc67352fe62a6edad
maxcolliander <129760842+maxcolliander@users.noreply.github.com>
2024-03-06 14:04:08 +0100

    Added so code is displayed properly
    All coding languages except html works also minor fix to display focus / calm levels

--- AIHandler.js
+++ AIHandler.js
@@ -11,9 +11,9 @@ const { retrieveResponse } = require('./OpenAIExtension.js');
 dotenv.config();
 
 // Constants, Standard messages to promt against chatGPT
-const MESSAGE_HELP_UNFOCUSED_DEV = "Help a programmer who is at a certain focus level to become more focused. Give 3 example of what he can do to become more focused with two sentances. Use the focus level in your message once";
+const MESSAGE_HELP_UNFOCUSED_DEV = "Help a programmer who is at a certain focus level to become more focused. Give 3 example of what he can do to become more focused with two sentances. You must use the focus level in your message once";
 const MESSAGE_TAKE_BREAK = "Generate a friendly messsage telling a developer to take a short brake. 2 sentances. ";
-const MESSAGE_CALM_DOWN = "Help a programmer who is at a certain calm level to calm down in a friendly manner, but not weird. 2 sentaces. Use the calm level in your message once";
+const MESSAGE_CALM_DOWN = "Help a programmer who is at a certain calm level to calm down in a friendly manner, but not weird. 2 sentaces. You must use the calm level in your message once";
 const CONTEXT_HELPADEV = "You are a helpful AI assitant with the goal to boost a developers productivity and focus. Short respones.";
 
 class AIHandler {
@@ -44,7 +44,7 @@ class AIHandler {
         
     }
     //Typ klar behövs test
-    async sendMsgToAI(preset,msg, chatactive){
+    async sendMsgToAI(preset, msg, chatactive){
         var templist = []
         if (chatactive == true){
             preset += ",This is the code assistant (you) previous message: " + this.aipreviousmsg;
@@ -55,25 +55,27 @@ class AIHandler {
             this.output = templist[0].join('');
             if (chatactive == true) {
                 this.aipreviousmsg = this.output;
+                this.output = this.output.replace(/```/, "<code>").replace(/```/, "</code>");
             }
             
         });
     };
     async sendMsgToUnfocusedDev(focus){
         focus *= 100;
-        focus.toFixed(0);
+        focus = focus.toFixed(0);
+        console.log(focus);
         focus = "This is the focus level in percent:  " + focus.toString() + "%. ";
         console.log(focus);
         // sends a message to the AI with standard message telling the user to focus. 
-        await this.sendMsgToAI(CONTEXT_HELPADEV, focus + MESSAGE_HELP_UNFOCUSED_DEV, false);
+        await this.sendMsgToAI(CONTEXT_HELPADEV, MESSAGE_HELP_UNFOCUSED_DEV + focus, false);
     }
     async sendMsgToAggitatedDev(calm){
         // sends a message to the AI with standard message telling the user to calm down. 
         calm *= 100;
-        calm.toFixed(0);
+        calm = calm.toFixed(0);
         calm = "This is the calm level in percent:  " + calm.toString() + "%. ";
         console.log(calm);
-        await this.sendMsgToAI(CONTEXT_HELPADEV , calm + MESSAGE_CALM_DOWN, false);
+        await this.sendMsgToAI(CONTEXT_HELPADEV , MESSAGE_CALM_DOWN + calm, false);
     }  
     async sendMsgToTakeBreak(){
         // sends a message to the AI with standard message telling the user to take a break.
