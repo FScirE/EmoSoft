11461556700afd1ffb348f2a3b76bd1707563e2f
FScirE <leo.soleke@gmail.com>
2024-03-20 11:16:45 +0100

    moved code to eyetracker.js, can return line contents

--- Eyetracker.js
+++ Eyetracker.js
@@ -1,9 +1,19 @@
 
 const net = require('net')
+const vscode = require('vscode')
 const { exec } = require('child_process')
 var DOMParser = require('xmldom').DOMParser;
 
 const MAX_LENGTH = 60
+
+const EDITOR_START_Y = 0.107
+const EDITOR_END_Y = 0.733
+const EDITOR_START_X = 0.135
+const LINE_HEIGHT = (EDITOR_END_Y - EDITOR_START_Y) / 30 //assume 30 lines
+
+const decorationType = vscode.window.createTextEditorDecorationType({
+    backgroundColor: 'rgba(200, 200, 200, 0.3)'
+})
 
 class EyeTracker {
     constructor(path) {
@@ -61,8 +71,61 @@ class EyeTracker {
         return this.Y.reduce((a, b) => a + b, 0) / this.Y.length
     }
 
+    getSetLinesInFocus() {
+        var editor = vscode.window.visibleTextEditors[0]
+		var decorationRange = []
+        var returnText = ''
+
+        //console.log("X: " + this.eyetracker.getX())
+		//console.log("Y: " + this.eyetracker.getY() + "\n")
+
+		if (editor != undefined) {	
+			//var y = this.getY()
+			//var x = this.getX()
+            var y = 0.12
+            var x = 0.21
+			if (y >= EDITOR_START_Y && y <= EDITOR_END_Y && x >= EDITOR_START_X) {
+
+				var currentRange = editor.visibleRanges
+
+				var current = Math.floor(((y - EDITOR_START_Y) * 30) / (EDITOR_END_Y - EDITOR_START_Y)) //assume 30 lines	
+                if (current < 0) current = 0 //avoid negative lines			
+				var lineNumber = currentRange[0].start.line + current
+				var line = editor.document.lineAt(lineNumber)
+				
+				if (!line.isEmptyOrWhitespace) {
+                    var startLine = current == 0 ? lineNumber : lineNumber - 1
+                    var endLine = current == 29 ? lineNumber : lineNumber + 1
+
+					var start = new vscode.Position(startLine, 0);
+					var end = new vscode.Position(endLine, editor.document.lineAt(endLine).text.length);
+					var range = new vscode.Range(start, end);
+
+					decorationRange = [range]
+                    returnText = editor.document.getText(range) //return text ofr the 3 lines
+				}
+			}
+			editor.setDecorations(decorationType, decorationRange)
+		}
+        return returnText
+
+        //x:0.1347 editor start
+		//y:0.1076 editor start
+		//y:0.7333 editor end
+		//assume 30 lines visible
+    }
+
     generateHeatMap() {
         exec(`python heatmapGenerator.py ${this.long_X.toString()} ${this.long_Y.toString()}`, {cwd: this.path})
+    }
+
+    calibrate() {
+        this.socket.write(
+            '<SET ID="CALIBRATE_SHOW" VALUE="1" />\r\n' + 
+            //'<SET ID="CALIBRATE_RESET" />\r\n' + kanske inte behövs
+            '<SET ID="CALIBRATE_START" VALUE="1" />\r\n')
+        //wait for calibrate to send finish message ------
+        this.socket.write('<SET ID="CALIBRATE_SHOW" VALUE="0" />\r\n')
     }
 }
 
--- extension.js
+++ extension.js
@@ -1,11 +1,5 @@
 // The module 'vscode' contains the VS Code extensibility API
 
-//TESTING
-const EDITOR_START_Y = 0.107
-const EDITOR_END_Y = 0.733
-const EDITOR_START_X = 0.135
-const LINE_HEIGHT = (EDITOR_END_Y - EDITOR_START_Y) / 30 //assume 30 lines
-
 
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
@@ -56,12 +50,6 @@ async function activate(context) {
 
 	this.eyetracker = new EyeTracker(context.extensionPath)
 
-	//test --------------------------------
-	const decorationType = vscode.window.createTextEditorDecorationType({
-		backgroundColor: 'rgba(200, 200, 200, 0.3)'
-	})
-	//-------------------------------------
-
 	setInterval(async () => {
 		var calm = await this.dataHandler.getCalm()
 		var focus = await this.dataHandler.getFocus()
@@ -71,46 +59,10 @@ async function activate(context) {
 			this.uiHandler.setFocusProgress(focus)
 		}
 
-		//await this.eventHandler.checkCalm(calm);
-		//await this.eventHandler.checkFocus(focus);
+		await this.eventHandler.checkCalm(calm);
+		await this.eventHandler.checkFocus(focus);
 
-		//console.log("X: " + this.eyetracker.getX())
-		//console.log("Y: " + this.eyetracker.getY() + "\n")
-
-		//test --------------------------------
-		var editor = vscode.window.visibleTextEditors[0]
-		var decorationRange = []
-
-		if (editor != undefined) {	
-			var y = this.eyetracker.getY()
-			var x = this.eyetracker.getX()
-			if (y >= EDITOR_START_Y && y <= EDITOR_END_Y && x >= EDITOR_START_X) {
-
-				var currentRange = editor.visibleRanges
-
-				var current = Math.floor(((y - EDITOR_START_Y) * 30) / (EDITOR_END_Y - EDITOR_START_Y)) //assume 30 lines				
-				var lineNumber = currentRange[0].start.line + current
-				var line = editor.document.lineAt(lineNumber)
-				
-				if (!line.isEmptyOrWhitespace) {
-					var start = new vscode.Position(lineNumber - 1, 0);
-					var end = new vscode.Position(lineNumber + 1, line.text.length);
-					var range = new vscode.Range(start, end);
-
-					decorationRange = [range]
-				}
-			}
-			editor.setDecorations(decorationType, decorationRange)
-		}
-		//x:0.23 explorer/editor
-		//x:0.73 editor/extension
-		//y:0.10 head/editor
-		//y:0.60 editor/terminal
-
-		//y:0.1076 editor start
-		//y:0.7333 editor end
-		//assume 30 lines visible
-		//----------------------------------------
+		this.eyetracker.getSetLinesInFocus()
 
 	}, 500);
 
