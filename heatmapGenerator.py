4710ce3594ab85b9aff073410b10efd1749a0b6f
FScirE <leo.soleke@gmail.com>
2024-05-15 16:52:40 +0200

    heatmap improvement

--- heatmapGenerator.py
+++ heatmapGenerator.py
@@ -23,11 +23,11 @@ for arg in sys.argv[1:]:
     this_cwd += arg + ' '
 this_cwd = this_cwd.rstrip()
 
-x_arr = list(map(float, read_file(this_cwd + '\\xValues.txt').split(','))) 
-y_arr = list(map(float, read_file(this_cwd + '\\yValues.txt').split(',')))
-#import random
-#x_arr = [random.random() for _ in range(10000)] # test
-#y_arr = [random.random() for _ in range(10000)] # test
+# x_arr = list(map(float, read_file(this_cwd + '\\xValues.txt').split(',')))
+# y_arr = list(map(float, read_file(this_cwd + '\\yValues.txt').split(',')))
+import random
+x_arr = [random.random() for _ in range(10000)] # test
+y_arr = [random.random() for _ in range(10000)] # test
 
 
 def calculate_intensity(x_in, y_in):
@@ -37,7 +37,7 @@ def calculate_intensity(x_in, y_in):
         intensity[math.floor(y_in[i]*GRID_HEIGHT)][math.floor(x_in[i]*GRID_WIDTH)] += 1
 
     return intensity
-                               
+
 def generate_heatmap(x_in, y_in):
     """Generates a heatmap, stores it as heatmap.jpg. x_in and y_in are arrays containing the x and y values. """
     intensity = calculate_intensity(x_in, y_in)
@@ -51,14 +51,29 @@ def generate_heatmap(x_in, y_in):
 
     cmap = clr.LinearSegmentedColormap.from_list(name='transparent-red', colors=[(0, 0, 0, 0), (0.33, 0, 0, 0.33), (0.67, 0, 0, 0.67), (1, 0, 0, 1), (1, 1, 1, 1)])
     plt.imshow(intensity, cmap=cmap, interpolation='catrom', extent=[0, GRID_WIDTH, GRID_HEIGHT, 0])
-       
+
+    max_val = max([max(e) for e in intensity]) #get highest value of all points
+
     cbar = plt.colorbar()  # Add color bar indicating the scale
-    cbar.set_ticks([0, 1])
+    cbar.set_ticks([0, max_val])
     cbar.set_ticklabels(['Min', 'Max'])
 
     # Remove x- and y-labels on the heatmap
-    plt.gca().set_xticklabels([])
+    plt.gca().set_yticks([])
     plt.gca().set_yticklabels([])
+    plt.gca().set_xticks([])
+    plt.gca().set_xticklabels([])
+
+    # heatmap border set to white
+    plt.gca().spines['top'].set_color('white')
+    plt.gca().spines['bottom'].set_color('white')
+    plt.gca().spines['left'].set_color('white')
+    plt.gca().spines['right'].set_color('white')
+
+    # colorbar border and labels set to white
+    cbar.ax.yaxis.set_tick_params(color='white')
+    plt.setp(plt.getp(cbar.ax.axes, 'yticklabels'), color='white')
+    cbar.outline.set_edgecolor('white')
 
     #plt.show()
     plt.savefig("heatmaps/heatmap.png", transparent=True)
